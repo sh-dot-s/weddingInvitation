dd5432650f5159576ebb7f0c3938253df5b18744
Sankarasetty Sri Harsha <sankarasettysriharsha@192.168.1.2>
2019-02-10 15:24:21 +0530

    Deployment Bug Fix

--- scripts/app.js
+++ scripts/app.js
@@ -2,6 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Base from "./components/Base";
 import './styles/stylesheet.scss';
-import 'mapbox-gl/dist/mapbox-gl.css';
 
 ReactDOM.render(<Base/>, document.getElementById("app"));
--- scripts/components/Details.js
+++ scripts/components/Details.js
@@ -76,7 +76,7 @@ class Details extends React.Component {
                                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3808.8690687453036!2d78.55518381403799!3d17.321865309252804!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcba21a25a803dd%3A0xe7ff2e1c075a1525!2sB+D+Reddy+Gardens!5e0!3m2!1sen!2sin!4v1549786252896" width="100%" height="100%" frameBorder="0" style={{border:0}} allowFullScreen></iframe>                    
                             {/* </CardMedia> */}
                             <CardActions>
-                                <Button variant="raised" size="large" href={this.state.wedurl} target="_blank">Take me to Wedding Venue</Button>
+                                <Button variant="contained" size="large" href={this.state.wedurl} target="_blank">Take me to Wedding Venue</Button>
                             </CardActions>
                         </Card>
                     </Grid>
@@ -95,7 +95,7 @@ class Details extends React.Component {
                                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3830.625372600718!2d80.63998431402257!3d16.239688139162713!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a4a0605db92bf75%3A0x616019601f1e2ee6!2sGoutham+Grand+Hotel!5e0!3m2!1sen!2sin!4v1549783371563" width="100%" height="100%" frameBorder="0" style={{border:0}} allowFullScreen></iframe>
                             {/* </CardMedia> */}
                             <CardActions>
-                                <Button variant="raised" size="large" href={this.state.recurl} target="_blank">Take me to Reception Venue</Button>
+                                <Button variant="contained" size="large" href={this.state.recurl} target="_blank">Take me to Reception Venue</Button>
                             </CardActions>
                         </Card>
                     </Grid>
